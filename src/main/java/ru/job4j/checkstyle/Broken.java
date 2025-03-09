cc7f19c0f35e7d507f2d4da5420ec197f1583c38
RRMermaid <rnizametdinova1991@gmail.com>
2025-03-10 01:35:10 +0500

    6. Правила Checkstyle [#504981 #534103]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,28 +1,41 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    private final int sizeOfEmpty = 10;
 
-    public String surname;
+    private final int sizeOfEmpty = 10;
+    private String surname;
+    private String name;
 
     public static final String NEW_VALUE = "";
 
-    String name;
-
-    void echo() {
+    public Broken() {
     }
 
-    void media(Object obj) {
+    public void echo() {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
 
-        }
+    public String getSurname() {
+        return surname;
+    }
 
-        Broken() {
-        }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
