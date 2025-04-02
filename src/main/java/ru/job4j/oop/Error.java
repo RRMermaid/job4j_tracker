a5670a69c819f63a30bc6544a008cc3b40fcb147
RRMermaid <rnizametdinova1991@gmail.com>
2025-04-03 00:19:08 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,40 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.active = false;
+        this.status = 0;
+        this.message = "No message";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printError() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        System.out.println("Default Error");
+        defaultError.printError();
+
+        Error customError1 = new Error(true, 404, "Page Not Found");
+        System.out.println("\nCustom Error 1:");
+        customError1.printError();
+
+        Error customError2 = new Error(false, 500, "Internal Server Error");
+        System.out.println("\nCustom Error 2:");
+        customError2.printError();
+    }
+}
