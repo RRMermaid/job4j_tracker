75879108f8ef99f096e1a7ada4cab55b4bf57389
RRMermaid <rnizametdinova1991@gmail.com>
2025-06-17 02:39:34 +0500

    12.0. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,5 +4,34 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
+    @Test
+    void whenCreateItem() {
+        Input input = new MockInput(new String[]{"0", "Item name", "1"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new CreateAction(), new ExitAction()};
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
 
+    @Test
+    void whenReplaceAction() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Old name"));
+        String[] answers = {"0", String.valueOf(item.getId()), "New name", "1"};
+        Input input = new MockInput(answers);
+        UserAction[] actions = {new ReplaceAction(), new ExitAction()};
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo("New name");
+    }
+
+    @Test
+    void whenDeleteAction() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Item to delete"));
+        String[] answers = {"0", String.valueOf(item.getId()), "1"};
+        Input input = new MockInput(answers);
+        UserAction[] actions = {new DeleteAction(), new ExitAction()};
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
 }
